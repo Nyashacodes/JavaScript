2af8bdde4880552a8623f130b652c03bea37ab8b
Nyashacodes <62797747+Nyashacodes@users.noreply.github.com>
2023-10-01 18:55:36 +0000

    will be back soon

---
+++ function.js
@@ -0,0 +1,16 @@
+// console.log("Working!")
+function sayMyName(){
+    console.log("N")
+    console.log("Y")
+    console.log("A")
+    console.log("S")
+    console.log("H")
+    console.log("A")
+}
+// sayMyName if i write this only it will be called as reference and sayMyName() is called execution
+sayMyName();
+
+function add(num1, num2){
+    console.log(num1+num2)
+}
+add(3, "a")
