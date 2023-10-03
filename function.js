5ead1e6cecc1a587568bc673c260d4dd74c9ae9a
Nyashacodes <62797747+Nyashacodes@users.noreply.github.com>
2023-10-03 01:03:21 +0000

    function

--- function.js
+++ function.js
@@ -10,7 +10,13 @@ function sayMyName(){
 // sayMyName if i write this only it will be called as reference and sayMyName() is called execution
 sayMyName();
 
-function add(num1, num2){
+function add(num1, num2){ // here while we define a function, whatever input we take is called parameter
     console.log(num1+num2)
 }
-add(3, "a")
+add(3, "a")//here the inputs are called arguments
+
+const Return = add(3,5);
+// console.log(Return);// will give us undefined because retun is not holding anything,because in function defination we did console.log we are not returnig anything. 
+
+
+//THE CONCEPT OF RETURN
